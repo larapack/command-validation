07ee5785450b3e915ebcc84c4c5d88722fcea991
Mark Topper <mail@webman.io>
2015-11-27 14:52:37 +0100

    Create Validateable.php

---
+++ src/Larapack/CommandValidation/Validateable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Larapack\CommandValidation;
+
+trait Validateable
+{
+	protected function validate($method, $callback)
+	{
+		$value = $method();
+		
+		$validate = $callback($value);
+		
+		if ($validate !== true && $validate !== null) {
+			if (is_string($validate)) {
+				$this->warn($validate);
+			}
+			
+			$value = $this->validate($method, $callback);
+		}
+		
+		return $value;
+	}
+}
