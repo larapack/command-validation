ecb4653a8ce5a0eb57b053d3c59ad487081f7f70
Mark Topper <mail@webman.io>
2016-03-04 04:15:07 -0500

    Applied fixes from StyleCI

--- src/Larapack/CommandValidation/Validateable.php
+++ src/Larapack/CommandValidation/Validateable.php
@@ -4,20 +4,20 @@ namespace Larapack\CommandValidation;
 
 trait Validateable
 {
-	protected function validate($method, $callback)
-	{
-		$value = $method();
-		
-		$validate = $callback($value);
-		
-		if ($validate !== true && $validate !== null) {
-			if (is_string($validate)) {
-				$this->warn($validate);
-			}
-			
-			$value = $this->validate($method, $callback);
-		}
-		
-		return $value;
-	}
+    protected function validate($method, $callback)
+    {
+        $value = $method();
+
+        $validate = $callback($value);
+
+        if ($validate !== true && $validate !== null) {
+            if (is_string($validate)) {
+                $this->warn($validate);
+            }
+
+            $value = $this->validate($method, $callback);
+        }
+
+        return $value;
+    }
 }
